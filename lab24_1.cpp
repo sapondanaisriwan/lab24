4a25811eb55e2c12fd5e05d45b57924967ca292a
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-03-02 17:25:41 +0700

    Refactor Node and List classes for improved readability and add remove method in List class

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -1,61 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
+class Node
+{
+public:
+	int data;
+	Node *next;
+	~Node();
 };
 
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
+class List
+{
+public:
+	Node *root;
+	int size;
+	void show();
+	void append(int);
+	void insert(int, int);
+	void remove(int);
 };
 
-Node::~Node(){
-    cout << data << " was deleted.\n";
+Node::~Node()
+{
+	cout << data << " was deleted.\n";
 }
 
-void List::insert(int d,int idx){	
+void List::insert(int d, int idx)
+{
 	Node *n = new Node;
 	n->data = d;
-	
-	if(idx == 0){
+
+	if (idx == 0)
+	{
 		n->next = root;
 		root = n;
 		return;
 	}
 	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
+	for (int i = 0; i < idx - 1; i++)
+	{
 		current = current->next;
 	}
 	n->next = current->next;
-	current->next = n;		
+	current->next = n;
 }
 
-void List::show(){
+void List::show()
+{
 	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
+	cout << current->data << " ";
+	while (current->next)
+	{
 		current = current->next;
 		cout << current->data << " ";
-	}	
+	}
 }
 
-void List::append(int d){	
+void List::append(int d)
+{
 	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
+	n->data = d;
+	n->next = NULL;
+	if (root == NULL)
+		root = n;
+	else
+	{
 		Node *current = root;
-		while(current->next){
+		// cout << current->next;
+		while (current->next)
+		{
 			current = current->next;
 		}
 		current->next = n;
@@ -63,4 +76,53 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+// 5 7 11 4 12
+void List::remove(int index)
+{
+	size--;
+	if (index == 0)
+	{
+		Node *temp = root;
+		root = root->next;
+		delete temp;
+		return;
+	}
+
+	Node *current = root;
+	// หาโหนดก่อนหน้าโหนดที่ต้องการลบ
+	for (int i = 0; i < index - 1; i++)
+	{
+		current = current->next;
+	}
+
+	// ลบโหนดเป้าหมาย
+	Node *nodeToDelete = current->next;
+	current->next = nodeToDelete->next;
+	delete nodeToDelete;
+};
+
+int main()
+{
+
+	List myList = {0, 0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+	myList.show();
+	cout << "\n";
+
+	myList.remove(0);
+	myList.show();
+	// cout << "\n";
+	// myList.remove(4);
+	// myList.show();
+	// cout << "\n";
+	// myList.remove(0);
+	// myList.show();
+	// cout << "\n";
+
+	return 0;
+}
--- lab24_2.cpp
+++ lab24_2.cpp
@@ -1,31 +1,65 @@
-#include<iostream>
-#include<list>
-#include<algorithm>
+#include <iostream>
+#include <list>
+#include <algorithm>
 
 using namespace std;
 
-void printList(list<string> s){
+void printList(list<string> s)
+{
 	list<string>::iterator i;
-	for( i = s.begin(); i != s.end(); i++){
+	for (i = s.begin(); i != s.end(); i++)
+	{
 		cout << *i << " ";
-	}		
-	cout << "\n";	
+	}
+	cout << "\n";
 }
 
-int main(){
-	
+int main()
+{
+
 	list<string> line_up;
 	list<string>::iterator loc;
-	
+
+	// Write your code here
+
+	// Alice ปวดขี้และมายืนรอคิวเป็นคนแรก
 	line_up.push_back("Alice");
+	// Bob มาต่อแถว
 	line_up.push_back("Bob");
+
+	loc = find(line_up.begin(), line_up.end(), "Bob");
+	// Oscar มาแซงแถวหน้า Bob
+	line_up.insert(loc, "Oscar");
+
+	// Luffy, Sanji, Nami มาต่อแถวด้านหลังตามลำดับ
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	// 2 คนที่หัวแถวได้เข้าไปในส้วมแล้ว
+	line_up.pop_front();
+	line_up.pop_front();
+
+	// Narutu มาแทรกแถวหลัง Luffy
+	loc = find(line_up.begin(), line_up.end(), "Sanji");
+	line_up.insert(loc, "Narutu");
+
+	// Prayath ท้องเสียปวดขี้สุด ๆ เลยมาแทรกแซวหน้าสุด
+	line_up.push_front("Prayath");
+	// Tony ยอมไม่ได้เลยมาแทรกด้านหลัง Prayath
+	loc = find(line_up.begin(), line_up.end(), "Prayath");
+	line_up.insert(loc, "Tony");
+
+	// Bob ทนไม่ไหวเลย เลยเปลี่ยนใจออกไปขี้ที่อื่น
+	loc = find(line_up.begin(), line_up.end(), "Bob");
+	line_up.erase(loc);
+	// 3 คนที่หัวแถวได้เข้าไปในส้วมแล้ว
+	line_up.pop_front();
+	line_up.pop_front();
+	line_up.pop_front();
 	
-	loc = find(line_up.begin(),line_up.end(),"Bob");
-	line_up.insert(loc,"Oscar");
-	
-	//Write your code here
-	
+	// สุดท้าย Function printList() จะแสดงลำดับของผู้ที่ยังเหลืออยู่ในคิวหลังจากเหตุการณ์ที่กำหนดด้านบน
 	printList(line_up);
-		
+
 	return 0;
 }
